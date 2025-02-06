147a3ad04b21b9e1922896aa3f142f9ff80827af
nitish <ns710844@gmail.com>
2025-02-06 20:43:39 +0530

    Roles based access done.

--- backend/api-gateway/gateway.js
+++ backend/api-gateway/gateway.js
@@ -12,8 +12,7 @@ const JWT_SECRET = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0
 const upload = multer();
 
 const SERVICES = {
-    auth: 'http://localhost:4000/auth',
-    user: 'http://localhost:4001/user',
+    user: 'http://localhost:4001/api/user',
     product: 'http://localhost:4002/product',
 }
 
@@ -22,12 +21,22 @@ function verifyToken(req, res, next) {
     if (!token) {
         return res.status(401).json({ error: 'Token is required' });
     }
-    jwt.verify(token, JWT_SECRET, (err) => {
-        if (err) {
-            return res.status(401).json({ error: 'Token is invalid' });
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+      } catch (err) {
+        return res.status(401).json({ message: 'Invalid token.' });
+      }
+}
+
+const checkRole = (roles)=>{
+    return (req, res, next) =>{
+        if(!roles.includes(req.user.userRole)){
+            return res.status(403).json({ message: 'Access denied. You do not have permission.' });
         }
         next();
-    })
+    }
 }
 
 function createFormData(req) {
@@ -55,7 +64,7 @@ function createFormData(req) {
 }
 
 //product routes
-app.post('/product/add-category', verifyToken, upload.any(), (req, res) => {
+app.post('/product/add-category', verifyToken, checkRole([1]), upload.any(), (req, res) => {
     const formData = createFormData(req);
     axios.post(`${SERVICES.product}/add-category`, formData, {
         headers: {
@@ -66,14 +75,12 @@ app.post('/product/add-category', verifyToken, upload.any(), (req, res) => {
     .then(response => res.status(200).json(response.data))
     .catch(error => handleError(error, res));
 });
-
-app.get('/product/get-category/:id?', verifyToken, (req, res) => {
+app.get('/product/get-category/:id?', verifyToken, checkRole([1,2]), (req, res) => {
     axios.get(`${SERVICES.product}/get-category/${req.params.id || ''}`)
         .then(response => res.status(200).json(response.data))
         .catch(error => handleError(error, res));
 });
-
-app.post('/product/add-product', verifyToken, upload.any(), (req, res) => {
+app.post('/product/add-product', verifyToken, checkRole([1]), upload.any(), (req, res) => {
     const formData = createFormData(req);
     axios.post(`${SERVICES.product}/add-product`, formData, {
         headers: {
@@ -84,26 +91,22 @@ app.post('/product/add-product', verifyToken, upload.any(), (req, res) => {
     .then(response => res.status(200).json(response.data))
     .catch(error => handleError(error, res));
 });
-
-app.get('/product/get-product/:id?', verifyToken, (req, res) => {
+app.get('/product/get-product/:id?', verifyToken, checkRole([1,2]), (req, res) => {
     axios.get(`${SERVICES.product}/get-product/${req.params.id || ''}`)
         .then(response => res.status(200).json(response.data))
         .catch(error => handleError(error, res));
 });
-
-app.get('/product/get-product-by-category/:id?', verifyToken, (req, res) => {
+app.get('/product/get-product-by-category/:id?', verifyToken, checkRole([1,2]), (req, res) => {
     axios.get(`${SERVICES.product}/get-product-by-category/${req.params.id || ''}`)
         .then(response => res.status(200).json(response.data))
         .catch(error => handleError(error, res));
 });
-
-app.get('/product/get-brand/:id?', verifyToken, (req, res) => {
+app.get('/product/get-brand/:id?', verifyToken, checkRole([1,2]), (req, res) => {
     axios.get(`${SERVICES.product}/get-brand/${req.params.id || ''}`)
         .then(response => res.status(200).json(response.data))
         .catch(error => handleError(error, res));
 });
-
-app.post('/product/add-brand', verifyToken, (req, res) => {
+app.post('/product/add-brand', verifyToken, checkRole([1]), (req, res) => {
     const formData = createFormData(req);
     axios.post(`${SERVICES.product}/add-brand`, formData, {
         headers: {
@@ -115,10 +118,9 @@ app.post('/product/add-brand', verifyToken, (req, res) => {
     .catch(error => handleError(error, res));
 });
 
-
 //user routes
-app.get('/user/:id?', verifyToken, (req, res) => {
-    axios.get(`${SERVICES.product}/:id?/${req.params.id || ''}`)
+app.get('/api/user/:id?', verifyToken, checkRole([1,2]), (req, res) => {
+    axios.get(`${SERVICES.user}/${req.params.id || ''}`)
         .then(response => res.status(200).json(response.data))
         .catch(error => handleError(error, res));
 });
@@ -127,17 +129,14 @@ app.get('/user/:id?', verifyToken, (req, res) => {
 function handleError(error, res) {
     console.error('Error occurred:', error.message);
     if (error.response) {
-        // request made, but server responded with an error
         return res.status(error.response.status).json({
             message: error.response.data.message || 'Service error occurred',
         });
     } else if (error.request) {
-        // request made but no response was received
         return res.status(500).json({
             message: 'No response from service, try again later',
         });
     } else {
-        // Something else happened while setting up the request
         return res.status(500).json({
             message: error.message,
         });
